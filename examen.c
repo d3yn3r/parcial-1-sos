0937f13dce163e1cd2ee9bfea720a19483821e28
Juanes8a <juochoag@unal.edu.co>
2022-05-04 07:46:12 -0500

    diferencias

--- examen.c
+++ examen.c
@@ -24,20 +24,23 @@ int main(int argc, char* argv[]){
         struct timeval t_inicial;
         gettimeofday(&t_inicial,NULL);
         printf("El tiempo incial es de : %ld segundos\n ",t_inicial.tv_sec);
-   
-        write(fildes[1], &t_inicial, sizeof(float));
+        printf("ti del hijo: %ld\n",t_inicial.tv_usec);
+
+        write(fildes[1], &t_inicial, sizeof(t_inicial));
         //execlp("/root/examen","examen","-l",NULL);
         execlp(argv[1], argv[1],NULL);
     } else{
         //Proceso padre con espera de terminación del  proceso hijo 
         wait(NULL);
         struct timeval ti;
-        read(fildes[0],&ti,sizeof(float));
+        read(fildes[0],&ti,sizeof(ti));
+        printf("ti: %ld\n",ti.tv_sec);
+        printf("ti micro: %ld\n",ti.tv_usec);
         struct timeval t_final;
         gettimeofday(&t_final,NULL);
         printf("El tiempo final es de : %ld segundos\n",t_final.tv_sec);
 
-        float resultado_asaroso = (t_final.tv_sec - ti.tv_sec)+ 1e-9(t_final.tv_sec - ti.tv_sec);
+        float resultado_asaroso = (float)(t_final.tv_sec - ti.tv_sec)+ (float)1e-6*(t_final.tv_usec - ti.tv_usec);
 
         printf("El tiempo es : %ld\n",resultado_asaroso);
 
