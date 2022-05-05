c6fa821dc691524d3128cab2ee1646608d0f00ea
Juanes8a <juochoag@unal.edu.co>
2022-05-05 12:56:14 -0500

    Comentarios

--- examen.c
+++ examen.c
@@ -1,7 +1,7 @@
 /*                                  Sistemas operativos
             Parcial 1
             Profesor : Juan Felipe Muñoz Hernandez
-            Integrantes : Juan Esteban Ochoa CC: 
+            Integrantes : Juan Esteban Ochoa CC: 1.000.634.602
                           Deyner Elias Lopez Pineda CC: 1.216.728.753
 */
 
@@ -25,28 +25,34 @@ int main(int argc, char* argv[]){
     if (pid < 0){
         printf("¡Oh no!, falló el fork");
     } else if (pid ==0){    //Creación proceso hijo
-        //obtenemos el tiempo inicial
+
+        //Obtenemos el tiempo inicial
         struct timeval t_inicial;
         gettimeofday(&t_inicial,NULL);
-        //enviamos mediante una tuberia la estructura del tiempo inicial
+
+        //Enviamos mediante una tuberia la estructura del tiempo inicial
         write(fildes[1], &t_inicial, sizeof(t_inicial));
         execlp(argv[1], argv[1],NULL);
     } else{
         //Proceso padre con espera de terminación del  proceso hijo 
         wait(NULL);
-        //recuperamos la estructura enviada mediante la tuberia
+
+        //Recuperamos la estructura enviada mediante la tuberia
         struct timeval ti;
         read(fildes[0],&ti,sizeof(ti));
-        //calculamos el tiempo inicial
+
+        //Calculamos el tiempo inicial
         double total_inicial = ti.tv_sec + ti.tv_usec*1e-6;
-        printf("tiempo inicial es: %0.8f\n",total_inicial);
-        //obtenemos el tiempo final
+        printf("El tiempo inicial es: %0.8f segundos\n",total_inicial);
+
+        //Obtenemos el tiempo final
         struct timeval t_final;
         gettimeofday(&t_final,NULL);
         //Calculamos el tiempo final
         double total_final =  t_final.tv_sec + t_final.tv_usec*1e-6;
-        printf("tiempo final es: %0.8f\n",total_final);
-        //calculamos el tiempo total de ejecucion
+        printf("El tiempo final es: %0.8f segundos\n",total_final);
+
+        //Calculamos el tiempo total de ejecucion
         double total = total_final - total_inicial;
         printf("El tiempo de ejecucion es : %0.8f\n",total);
     }
