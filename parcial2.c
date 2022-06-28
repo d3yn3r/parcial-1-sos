a759e99632cd333417981efbd84b82cd004aa43a
D3YN3R <deelopezpi@unal.edu.co>
2022-06-28 12:31:40 -0500

    decimal a binario

--- parcial2.c
+++ parcial2.c
@@ -4,10 +4,27 @@
             Integrantes : Juan Esteban Ochoa CC: 1.000.634.602
                           Deyner Elias Lopez Pineda CC: 1.216.728.753
 */
-
-
 #include <stdio.h>
-#include <stdlib.h>
+#include <conio.h>   
+int main() 
+{ 
+    int z,x,numero,n2=0; 
+    int binario[50]; 
+    binario[0]=0; 
+  
+    printf("Introduce el decimal a convertir: " ); 
+    scanf("%iu",&numero); 
 
+    for (x=0;x<50;x++) { 
+        binario[x]=numero%2; 
+        numero=numero/2; 
+    if(numero==0) 
+    break; 
+    } 
 
-int main(){
+    for(z=x;z>=0;z--) { 
+        printf("%d",binario[z]); 
+    } 
+
+    getch(); 
+}
