fcc41e99689b545147e8d26a2d2fa63437f7d7ad
Juanes8a <juochoag@unal.edu.co>
2022-06-28 12:18:40 -0500

    l
    decimal a binario

---
+++ parcial2.c
@@ -0,0 +1,13 @@
+/*                                  Sistemas operativos
+            Parcial 2
+            Profesor : Juan Felipe Muñoz Hernandez
+            Integrantes : Juan Esteban Ochoa CC: 1.000.634.602
+                          Deyner Elias Lopez Pineda CC: 1.216.728.753
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
