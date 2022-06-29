5e9e9c335a4c8663b8d8e635aef612f4fe1f94b8
D3YN3R <deelopezpi@unal.edu.co>
2022-06-29 04:26:46 -0500

    desplazamiento y paginacion agregados

--- parcial2.c
+++ parcial2.c
@@ -8,30 +8,71 @@
 #include <conio.h> 
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+
 int main() { 
     
     while (1){
         int numero; 
-        char binario;
+        int binario[32];
         char control;
-        int c,n,k;
-
-        scanf("%s",&control); 
+        int z,x,d,q,potencia=0,desplazamiento=0,pagina = 0,cont;
+        printf("Ingrese dirección virtual: ");
+        scanf("%s",&control);
+        
+        //tiempo inicial
+        struct timeval t_inicial;
+        gettimeofday(&t_inicial,NULL);
+        double total_inicial = t_inicial.tv_sec + t_inicial.tv_usec*1e-6;
+        
         if(control == 's'){
             break;
         }
         
         else if(control != 's'){
             numero = atoi(&control);
-            for (c=15;c>=0;c--){
-	            k = numero>>c;
-	            if (k&1){
-	               printf("1");
-	            }
-		        else {
-    	            printf("0");
-		        }
-	        }
+            for (int i = 0; i < 32; i++) {
+                binario[i] = numero%2;
+                numero = numero/2;
+            }
         }
+        //Desplazamiento
+        for(z=0;z<12;z++) { 
+            d = binario[z];
+            if(d==1){
+               potencia = pow(2,z);
+               desplazamiento = desplazamiento + potencia;
+            }
+        }
+        //paginación
+        for (int i = 12; i < 32; i++) {
+            int exponente = i - 12;
+            int valor = binario[i];
+            if(valor == 1){
+                potencia = pow(2,exponente);
+                pagina = pagina + potencia;
+            }
+        }
+        
+        
+        //tiempo final
+        struct timeval t_final;
+        gettimeofday(&t_final,NULL);
+        double total_final =  t_final.tv_sec + t_final.tv_usec*1e-6;
+        
+        double total = total_final - total_inicial;
+        
+        printf("Desplazamiento: ");
+        printf("%d",desplazamiento);
+        printf("\n");
+        printf("Número de página: ");
+        printf("%d",pagina);
+        printf("\n");
+        printf("Tiempo de traducción: ");
+        printf("%0.8f",total);
+        printf("\n");
     }       
 }
